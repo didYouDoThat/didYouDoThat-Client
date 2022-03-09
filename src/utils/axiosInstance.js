1457066bd9e765af7851ea221260a9a4cea46528
okcorn <jsok79now@gmail.com>
2022-03-10 06:39:32 +0900

    fix: update app.json scheme for releasing apk

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { SERVER_URI } from "@env";
 
 const instance = axios.create({
-  baseURL: `${SERVER_URI}`,
+  // baseURL: `${SERVER_URI}`,
+  baseURL: "http://didyoudothat1-env.eba-sviaqywd.ap-northeast-2.elasticbeanstalk.com/",
   withCredentials: true,
 });
 
