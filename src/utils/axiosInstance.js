60abc8db35b7f35e7551f34af62365e21f64c1d9
okcorn <jsok79now@gmail.com>
2022-03-11 19:35:44 +0000

    chore: update axios baseUrl

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { SERVER_URI } from "@env";
 
 const instance = axios.create({
-  // baseURL: `${SERVER_URI}`,
-  baseURL: "http://192.168.0.11:8000",
+  baseURL: `${SERVER_URI}`,
   withCredentials: true,
 });
 
